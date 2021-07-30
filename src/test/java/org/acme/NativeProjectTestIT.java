34df220838a89c41863f8e27a78502b388567ad5
fajar2021 <80533433+fajar2021@users.noreply.github.com>
2021-07-30 10:29:28 +0700

    Update and rename NativeGreetingResourceIT.java to NativeProjectTestIT.java

--- src/test/java/org/acme/NativeProjectTestIT.java
+++ src/test/java/org/acme/NativeProjectTestIT.java
@@ -3,7 +3,7 @@ package org.acme;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
+public class NativeProjectTestIT extends GreetingResourceTest {
 
     // Execute the same tests but in native mode.
 }
